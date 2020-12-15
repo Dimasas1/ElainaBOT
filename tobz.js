70b4083fb1aba15d1477e9fbdad346d938ef2927
Dimasas1 <69768749+Dimasas1@users.noreply.github.com>
2020-12-15 14:00:19 +0700

    Update tobz.js

--- tobz.js
+++ tobz.js
@@ -276,7 +276,7 @@ module.exports = tobz = async (tobz, message) => {
         const serial = sender.id
 
         const isAdmin = adminNumber.includes(sender.id)
-        const ownerNumber = '6281311850715@c.us'
+        const ownerNumber = '6283154964401c.us'
         const isOwner = ownerNumber.includes(sender.id)
         
         const isWhite = (chatId) => adminNumber.includes(chatId) ? true : false
